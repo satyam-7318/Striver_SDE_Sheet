c8321848de83a6a2470120cbe64a8ae4a9436ca8
Satyam Tiwari <satyamtiwari2016@gmail.com>
2022-07-06 22:26:54 +0530

    Create Minimum Platforms.cpp

---
+++ Day8 Greedy/Minimum Platforms.cpp
@@ -0,0 +1,43 @@
+https://practice.geeksforgeeks.org/problems/minimum-platforms-1587115620/1#
+
+
+
+class Solution{
+    public:
+    //Function to find the minimum number of platforms required at the
+    //railway station such that no train waits.
+    int findPlatform(int arr[], int dep[], int n)
+    {
+    	// Your code here
+    	
+    	sort(arr,arr+n);
+    	sort(dep,dep+n);
+    	
+    	
+    	int plat = 1;
+    	int maxi = 1;
+    	
+    	int i=1,j=0;
+    	
+    	
+    	while(i<n && j<n){
+    	    
+    	    
+    	    if(arr[i]<=dep[j]){
+    	        plat++;
+    	        
+    	        i++;
+    	    }
+    	    else if(arr[i] > dep[j]){
+    	        plat--;
+    	        j++;
+    	    }
+    	    
+    	    if(plat>maxi)
+    	      maxi = plat;
+    	    
+    	}
+    	
+    	return maxi;
+    }
+};
