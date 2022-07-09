32d40f21316b7d023792785c1244f245c1e7efc2
Satyam Tiwari <satyamtiwari2016@gmail.com>
2022-07-09 22:25:35 +0530

    Create Top k frequent elements.cpp

---
+++ Dayy12 Heap/Top k frequent elements.cpp
@@ -0,0 +1,48 @@
+https://leetcode.com/problems/top-k-frequent-elements/
+
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        
+        unordered_map<int,int> mp;
+        
+        
+        for(int i = 0;i<nums.size();i++){
+            
+            mp[nums[i]]++;
+            
+        }
+        
+        
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> minh;
+        
+        
+        for(auto i = mp.begin();i!=mp.end();i++){
+            
+            
+            minh.push({i->second,i->first});
+            
+            
+            if(minh.size()>k){
+                minh.pop();
+            }
+            
+            
+        }
+        
+        
+        vector<int> ans;
+        
+        
+        while(minh.size()>0){
+            
+            ans.push_back(minh.top().second);
+            minh.pop();
+            
+        }
+        
+        return ans;
+        
+    }
+};
