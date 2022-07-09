8280ecb23310898b433024fb05db3d251086f40e
Satyam Tiwari <satyamtiwari2016@gmail.com>
2022-07-09 22:12:53 +0530

    Create data stream median.cpp

---
+++ Dayy12 Heap/data stream median.cpp
@@ -0,0 +1,30 @@
+https://leetcode.com/problems/find-median-from-data-stream/
+
+
+class MedianFinder {
+    priority_queue<int> maxheap;//store sorted first half elements //for odd n it will have an extra element
+    priority_queue<int,vector<int>,greater<int>> minheap;//store sorted second half elements
+public:
+    MedianFinder() {}
+    
+    void addNum(int num) {
+        maxheap.push(num);
+        minheap.push(maxheap.top());
+        maxheap.pop();
+        if(maxheap.size() < minheap.size()){    //if minheap has more element than max heap
+            maxheap.push(minheap.top());
+            minheap.pop();
+        } 
+    }
+    
+    double findMedian() {
+        if(maxheap.size() > minheap.size()) return maxheap.top();
+        return (maxheap.top() + minheap.top())/2.0;
+    }
+};
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
