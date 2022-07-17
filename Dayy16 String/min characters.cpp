28fd9f2290ec1f55df47302066e4e3c1e2d79fad
Satyam Tiwari <satyamtiwari2016@gmail.com>
2022-07-17 21:31:44 +0530

    Create min characters.cpp

---
+++ Dayy16 String/min characters.cpp
@@ -0,0 +1,75 @@
+https://www.interviewbit.com/problems/minimum-characters-required-to-make-a-string-palindromic/
+
+
+
+int lps(string pat){
+
+    int M = pat.size();
+
+    int lps[M];
+
+    
+
+    int len = 0;
+
+    int i = 1;
+
+    lps[0] = 0;
+
+    
+
+    while(i < M){
+
+        if(pat[i] == pat[len]){
+
+            len++;
+
+            lps[i] = len;
+
+            i++;
+
+        }else{
+
+            if(len!=0){
+
+                len = lps[len-1];
+
+            }else{
+
+                lps[i] = 0;
+
+                i++;
+
+            }
+
+        }
+
+    }
+
+    
+
+    return lps[M-1];
+
+}
+
+
+
+int Solution::solve(string A) {
+
+    string B = A;
+
+    reverse(B.begin(), B.end());
+
+    
+
+    string pat = A+"$"+B;
+
+    
+
+    int len = lps(pat);
+
+    
+
+    return A.size() - len;
+
+}
