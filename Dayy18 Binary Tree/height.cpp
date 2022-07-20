e651cf6db7d2ab3974b9635b276a9c0f8d504c94
Satyam Tiwari <satyamtiwari2016@gmail.com>
2022-07-20 18:56:16 +0530

    Create height.cpp

---
+++ Dayy18 Binary Tree/height.cpp
@@ -0,0 +1,18 @@
+https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        
+        if(root==NULL)
+            return 0;
+        
+        int left = maxDepth(root->left);
+        int right = maxDepth(root->right);
+        
+        return 1 + max(left , right);
+        
+    }
+};
