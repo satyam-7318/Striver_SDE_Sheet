75ab72cfc7435c2a859edba0083649278d7b1421
Satyam Tiwari <satyamtiwari2016@gmail.com>
2022-07-20 18:53:42 +0530

    Create LevelOrder.cpp

---
+++ Dayy18 Binary Tree/LevelOrder.cpp
@@ -0,0 +1,47 @@
+https://leetcode.com/problems/binary-tree-level-order-traversal/
+
+
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        
+        
+        vector<vector<int>> ans;
+        
+        if(root==NULL)
+            return ans;
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        
+        while(!q.empty()){
+            
+            int size = q.size();
+            
+            vector<int> v;
+            
+            for(int i=0;i<size;i++){
+                
+                TreeNode* temp = q.front();
+                q.pop();
+                
+                v.push_back(temp->val);
+                
+                if(temp->left)
+                    q.push(temp->left);
+                
+                if(temp->right)
+                    q.push(temp->right);
+                
+            }
+            
+            ans.push_back(v);
+            
+        }
+        
+        return ans;
+        
+    }
+};
